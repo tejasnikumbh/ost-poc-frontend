58e7f35360038a174955b6b182cf688d8233c47a
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-05-14 18:29:37 +0530

    Quiz Instruction with auth

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,16 +1,40 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchProfile, VIEW_PROFILE} from './../actions/index';
-
+import {Link} from 'react-router-dom';
 
 class Profile extends Component {
+
+  renderProfile() {
+    const user = this.props.profile.data.user;
+    const quizMetaData = this.props.profile.data.quiz;
+    console.log(user, quizMetaData);
+    return (
+      <div className='container'>
+        <div className='header'> Header </div>
+        <div className='title'> User Details </div>
+        <div className='details'>
+            <div className='detail-item'> Email: {user.email} </div>
+            <div className='detail-item'>
+              Token Balance: {user.ost_details.token_balance}
+            </div>
+        </div>
+        <div className='title'> Quizzes Available </div>
+        <div className='details'>
+        <div className='detail-item'>
+            <Link to="/quiz/instruction"> {quizMetaData.title} </Link>
+        </div>
+        </div>
+      </div>
+    );
+  }
+
   componentDidMount() {
     const token = sessionStorage.getItem('x-auth');
     this.props.fetchProfile(token);
   }
 
   render() {
-    console.log(this.props);
     // Case when fetching profile
     if(_.isEmpty(this.props.profile.data) &&
     !this.props.profile.isAuthenticated) {
@@ -22,8 +46,7 @@ class Profile extends Component {
       return (<div> 401 Access Unauthorized </div>);
     }
     // Correct access
-    console.log(this.props.profile.data);
-    return(<div> Profile Details </div>);
+    return this.renderProfile();
   }
 }
 
--- src/components/quiz-instruction.js
+++ src/components/quiz-instruction.js
@@ -1,9 +1,55 @@
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchProfile} from './../actions/index';
+import {Link} from 'react-router-dom';
 
 class QuizInstruction extends Component {
+
+  renderQuizInstruction() {
+    const quiz = this.props.profile.data.quiz;
+    return (
+      <div className='container'>
+        <div className='header'> Header </div>
+        <div className='title'> Instructions </div>
+        <div className='content'>
+          Welcome to D-Quiz. You can participate in this quiz
+          by paying the participation fees charged in DPLT. The
+          top percentage (specified below) will be rewarded
+          variable amounts of tokens based on our performance
+          evaluatiom algorithm. The algorithm takes a variety of
+          factors into account, so try not to cheat. The quiz is
+          timed as well, so the faster you can answer, the better.
+        </div>
+        <div className='title'> Specifics </div>
+        <div className='detail-item'> Participation Fee: {quiz.participation_fee} DPLT</div>
+        <div className='detail-item'> Reward Amount: {quiz.reward_amount} DPLT</div>
+        <div className='detail-item'> Percentage Rewarded: {quiz.percentage_rewarded * 100} % </div>
+        <Link to={`/quiz/${quiz.id}`} className='btn-link'>
+          <button className='btn btn-primary'> Take Quiz </button>
+        </Link>
+      </div>
+    );
+  }
+
+  // For auth population and quiz meta data
+  componentDidMount() {
+    if(!this.props.profile.isAuthenticated) {
+      const token = sessionStorage.getItem('x-auth');
+      this.props.fetchProfile(token);
+    }
+  }
+
   render() {
-    return(<div> Quiz Instructions </div>);
+    // Case when unauthorized access
+    if(!this.props.profile.isAuthenticated) {
+      return (<div> 401 Access Unauthorized </div>);
+    }
+    return this.renderQuizInstruction();
   }
 }
 
-export default QuizInstruction;
+function mapStateToProps({profile}) {
+  return {profile}
+}
+
+export default connect(mapStateToProps, {fetchProfile})(QuizInstruction);
