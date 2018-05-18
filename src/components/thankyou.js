abd3b0734c96b3b8729fb42a20ca9e28cd9e711b
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-05-18 17:19:14 +0530

    Updated thankyou page

--- src/components/thankyou.js
+++ src/components/thankyou.js
@@ -2,9 +2,29 @@ import React, {Component} from 'react';
 import {Link, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-class Thankyou extends Component {
-  render() {
+import _ from 'lodash';
 
+class Thankyou extends Component {
+
+  renderMessage() {
+    const submission = this.props.submission.data;
+    var message = "Thanks for trying to submit a response.";
+    if(_.isEmpty(submission)) {
+        return <div> { message } </div>
+    }
+
+    const alreadyTaken = submission.alreadyTaken;
+    var message = "Thanks for taking the quiz. Scores will be updated soon";
+    if(alreadyTaken) {
+      message = submission.message;
+      message += ` at ${submission.quiz.time}.
+      Your score for this quiz was ${submission.quiz.score}.
+      Please try other quizzes`;
+    }
+    return (<div> {message} </div>);
+  }
+
+  render() {
     if(!sessionStorage.getItem('x-auth')) {
       return (
         <Redirect to="/login"/>
@@ -15,9 +35,9 @@ class Thankyou extends Component {
       <div className='sub-container-quiz'>
         <div className='header'> Quiz </div>
         <div className='body'>
-          <div className='title'> Thankyou </div>
+          <div className='title'> Feedback </div>
           <div className='content'>
-            Thank You for taking the quiz! Scores will be updated soon.
+            {this.renderMessage()}
           </div>
         </div>
         <div className='spacer'></div>
